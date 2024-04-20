ff97f509e3192f89da15ca348af917117776ee4e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-04-20 16:53:34 -0500

    Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,49 @@ dependencies {
     api("org.apache.commons:commons-compress:1.26.1")
     compileOnlyApi("org.jetbrains:annotations:24.1.0")
 }
+
+publishing {
+    repositories {
+        fun maven(name: String, releases: String, snapshots: String) {
+            maven {
+                this.name = name
+                url = uri(if (version.toString().endsWith("-SNAPSHOT")) snapshots else releases)
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+
+        maven(
+            "gaming32",
+            "https://maven.jemnetworks.com/releases",
+            "https://maven.jemnetworks.com/snapshots"
+        )
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name = project.name
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/Gaming32/j21-generators/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Gaming32"
+                    }
+                }
+            }
+        }
+    }
+}
